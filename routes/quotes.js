6f710670a3b05dcd5237c9ec26b972e38e5eb194
Glitch (ownerrez-api-tester) <none>
2024-01-19 17:20:25 +0000

    🎊📖 Updated with Glitch

--- routes/quotes.js
+++ routes/quotes.js
@@ -1,5 +1,6 @@
 const Joi = require("joi");
 const appHelper = require("../src/helpers");
+const dateHelper = require("../src/datetools");
 
 module.exports = [
   {
@@ -45,5 +46,93 @@ module.exports = [
         return response.body;
       });
     },
-  },    
+  },
+  {
+    method: "POST",
+    path: "/quotes/create",
+    options: {
+      description: "Create a quote",
+      notes: "Creates a quote ",
+      tags: ["api", "Quotes"],
+      validate: {
+        failAction: async (request, h, err) => {
+          // During development, log and respond with the full error.
+          console.log(err);
+          throw err;
+        },
+        query: Joi.object({
+          propertyId: Joi.number()
+            .required()
+            .default(1234)
+            .description("Property Id"),
+          start_date: Joi.date()
+            .required()
+            .iso()
+            .default(dateHelper.dateOnly(new Date(Date.now())))
+            .description("Arrival Date"),
+          end_date: Joi.date()
+            .required()
+            .iso()
+            .default(
+              dateHelper.dateOnly(dateHelper.addDays(new Date(Date.now()), 2))
+            )
+            .description("Departure Date"),
+        }),
+        payload: Joi.object({
+          adults: Joi.number()
+            .required()
+            .default(2)
+            .description("Number of adults"),
+          children: Joi.number()
+            .required()
+            .default(0)
+            .description("Number of children"),
+          infants: Joi.number()
+            .required()
+            .default(0)
+            .description("Number of infants"),
+          noAgreement: Joi.bool().default(true).description("No agreement"),
+          agreementId: Joi.number()
+            .required()
+            .default(1)
+            .description("Agreement Id"),
+          addCharges: Joi.bool().default(true).description("Add charges"),
+          createHoldBlock: Joi.bool()
+            .default(false)
+            .description("Create a hold block"),
+          sendEmail: Joi.bool().default(false).description("Send email"),
+          skipRuleValidation: Joi.bool()
+            .default(false)
+            .description("Skip validation rules"),
+        }),
+      },
+    },
+    handler: async (request, h) => {
+      const quoteDetails = {
+        arrival: dateHelper.dateOnly(request.query.start_date),
+        departure: dateHelper.dateOnly(request.query.end_date),
+        adults: request.payload.adults,
+        children: request.payload.children,
+        infants: request.payload.infants,
+        noAgreement: request.payload.noAgreement,
+        agreementId: request.payload.agreementId,
+      };
+      console.log(quoteDetails);
+      const urlParams = new URLSearchParams({
+        addCharges: request.payload.addCharges,
+        createHoldBlock: request.payload.createHoldBlock,
+        sendEmail: request.payload.sendEmail,
+        skipRuleValidation: request.payload.skipRuleValidation,
+      });
+      console.log(urlParams.toString());
+
+      return await appHelper.GeneralErrorHandlerFn(async () => {
+        const response = await appHelper.Post(
+          `${appHelper.LegacyV1BaseUrl}/quotes${urlParams.toString()}`,
+          quoteDetails
+        );
+        return response.body;
+      });
+    },
+  },
 ];
